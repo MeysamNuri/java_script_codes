54c640ebb0c06a3f5df215efb054bba545da74c5
Meysam nuri <meysamnuri1992@gmail.com>
2021-03-25 02:27:20 +0430

    event mouse

---
+++ java-rocket/mouse event typography/script.js
@@ -0,0 +1,24 @@
+let hero=document.querySelector(".hero")
+let text=hero.querySelector("h1")
+let walk=500; //500 px
+
+hero.addEventListener("mousemove",function(event){
+    // console.log(event);
+    const {offsetWidth:width,offsetHeight:height}=hero
+    let {offsetX:x,offsetY:y}=event
+    // console.log(width,height);
+
+    x+=event.target.offsetLeft
+    y+=event.target.offsetTop
+    const xWalk=Math.floor((x / width * walk) - (walk / 2))
+    const yWalk=Math.floor((y / height * walk) - (walk / 2))
+console.log(xWalk,yWalk);
+    text.style.textShadow=`
+    ${xWalk}px ${yWalk}px 0 rgba(238 ,82,83,.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(52 ,31,151,.7),
+    ${yWalk}px ${xWalk * -1}px 0 rgba(243 ,104,224,.7),
+    ${yWalk * -1}px ${xWalk}px 0 rgba(254 ,202,87,.7)
+   
+    `
+    
+})
